e565fddeadbf3ad0c12a9b23bbb455f9aba6de6f
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2022-08-27 18:42:28 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using DataAnnotationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataAnnotationExample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Employee EmployeeModel)
+        {
+            string Response = "";
+            if (ModelState.IsValid)
+            {
+                Response = "Employee Data Saved Successfully";
+            }
+            else
+            {
+                Response =ModelState.ToString();
+            }
+            ViewBag.Message = Response;
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationExample.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="First Name is required")]
+        [StringLength(10, MinimumLength = 2)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(10,MinimumLength =2)]
+        [DisplayName("Last Name")]
+
+        public string LastName { get; set; }
+        [Required]
+        [Range(18,60)]
+        [DisplayName("Age")]
+
+        public int Age { get; set; }
+        [StringLength(50,MinimumLength =10)]
+        [DisplayName("Address")]
+
+        public string Address { get; set; }
+        [RegularExpression("\\d{6}")]
+        [DisplayName("Pin Code")]
+
+        public string PinCode { get; set; }
+        [EmailAddress]
+        [DisplayName("Email Address")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Password required")]
+        public string Password { get; set; }
+        [Compare("Password")]
+        [DisplayName("Confirm Password")]
+
+        public string ConfirmPassword { get; set; }
+    }
+}
